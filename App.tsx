35ebed492f4acb887bc3d1c5737f5f21120a298a
Pholosho Seloane <35956639+pholoshos@users.noreply.github.com>
2022-06-10 21:26:35 +0200

    EDIT: code clean up

--- App.tsx
+++ App.tsx
@@ -1,27 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { useContext, useState } from 'react';
-import { StyleSheet, Text, TouchableHighlightComponent, View } from 'react-native';
-import { user } from './src/context/user';
-import { IUser } from './src/modal/user';
+import { StyleSheet, View } from 'react-native';
 import { Home } from './src/screens/home';
 import {theme} from './src/styles/theme'
 
 export default function App() {
-  const userContext = useContext(user)
-  
-  const [userData,setUser] = useState<IUser>({
-    username: 'Pholosho',
-    phone : '0720648709'
-  })
 
   return (
-    <user.Provider value={userData}>
     <View style={styles.container}>
       <Home></Home>
       <StatusBar style={theme.mode} />
     </View>
-    </user.Provider>
-
   );
 }
 
