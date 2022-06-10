6b1eea8bd0e966682baddd2a5e062834067fc4ec
Pholosho Seloane <35956639+pholoshos@users.noreply.github.com>
2022-06-10 22:21:55 +0200

    Feature : added more components

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,9 @@ export { Image } from "./image";
 export { Card } from "./card";
 export {ScrollView} from "./scrollView";
 export {Banner} from "./banner"
+export {Content} from "./content";
+export {ListView} from "./listView";
+export {Input } from "./input";
+export {Modal} from "./modal";
+export {Subtitle} from "./subtitle";
+export {Title} from "./title";
