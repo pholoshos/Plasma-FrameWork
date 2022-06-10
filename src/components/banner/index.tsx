2d73a0fd585a30dc5130bad090d17c1b7d953404
Pholosho Seloane <35956639+pholoshos@users.noreply.github.com>
2022-06-10 22:07:47 +0200

    Feature : added more components

---
+++ src/components/banner/index.tsx
@@ -0,0 +1,6 @@
+import React from "react"
+import { View } from "react-native"
+
+export  const Banner = ()=>{
+    return <View></View>
+} 
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,5 @@ export { Navbar } from "./navbar";
 export { Alert } from "./alert";
 export { Image } from "./image";
 export { Card } from "./card";
+export {ScrollView} from "./scrollView";
+export {Banner} from "./banner"
